3b7e2d9df99fe7a61ac50628d13b923ef9dd334e
darsh <dsinghoct@gmail.com>
2024-09-21 14:18:23 +0530

    sqlite3 introduction code added into python

--- intro/pythonsqliteintro.py
+++ intro/pythonsqliteintro.py
@@ -1,1 +1,11 @@
-print("hello")
+import sqlite3
+connect = sqlite3.connect("Employee.db")
+cursor = connect.cursor()
+cursor.execute('''create table if not exists Employee(name varchar(255), age int, salary int)''')
+cursor.execute('''insert into Employee(name,age,salary) values('Rohit', 35, 40000)''')
+cursor.execute('''insert into Employee(name,age,salary) values('Keshav', 24, 70000)''')
+connect.commit()
+cursor.execute('''select * from Employee''')
+list = cursor.fetchall()
+for x in list:
+    print(x)
